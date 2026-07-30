3aef1ad9b8ba6f1e013b249ae2daf987eedadc38
biazika <beatriz.gomes.souza@escola.pr.gov.br>
2026-07-29 22:36:05 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "cine";
+
+
+$conn = new mysql($host, $usuario, $senha, $banco);
+
+if($conn-> connect_error){
+   die("Erro na conexão: ". $conn->connect_error);
+}
